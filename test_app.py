f8f2b6cf5bdfba1b6d7ed98377a9b58955018bda
manoj723529 <jamimanojkumar135@gmail.com>
2026-09-08 18:46:15 +0530

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,14 @@
+from app import add, subtract, divide, is_even
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_is_even():
+    assert is_even(4) is True
+    assert is_even(3) is False
